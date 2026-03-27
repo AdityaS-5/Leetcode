3ac45882a9bc0829ea5cab3bacc01c93b7e66a88
Aditya S <160204309+AdityaS-5@users.noreply.github.com>
2026-03-27 21:35:08 +0530

    Time: 34 ms (60.12%), Space: 60.4 MB (9.2%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> l = new HashSet<>();
-        
+        List<List<Integer>> l = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
-            Set<Integer> st  = new HashSet<>();
-            for(int j =i+1;j<nums.length;j++){
-                int k = -(nums[i]+nums[j]);
-                if(st.contains(k)){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j<k){
+                long sum = (long)nums[i] + nums[j] + nums[k];
+                if(sum<0)j++;
+                else if(sum>0)k--;
+                else{
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
-                    temp.add(k);
-                    Collections.sort(temp);
+                    temp.add(nums[k]);
                     l.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
-                st.add(nums[j]);
             }
         }
-        List<List<Integer>> immutableList = List.copyOf(l);
-        return immutableList;
+        return l;
     }
 }
